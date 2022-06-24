5c567f435c26d0990dff4d617efdf58c67d9c39f
atishek22 <kumaratishek22@gmail.com>
2022-06-24 11:18:17 +0530

    Updated patch generator to use an iterator

--- utils/wsi/patches.py
+++ utils/wsi/patches.py
@@ -38,16 +38,15 @@ def generate_patch(source_file: Path,
     slide = openslide.OpenSlide(source_file)
     x, y = slide.dimensions
 
+    s = np.zeros((1024, 1024, 4), dtype=np.uint8)
     s_x = x // size[0]
     s_y = y // size[1]
 
-    patches = []
-
     for i in range(s_x):
         for j in range(s_y):
-            s = slide.read_region(
-                (s_x * size[0], s_y * size[1]), 0, size)
-            img = cv.cvtColor(np.array(s), cv.COLOR_RGB2GRAY)
+            s[:, :, :] = np.array(slide.read_region(
+                (i * size[0], j * size[1]), 0, size))
+            img = cv.cvtColor(s, cv.COLOR_RGB2GRAY)
             blur = cv.GaussianBlur(img, (5, 5), 0)
             ret3, th3 = cv.threshold(blur, 200, 255, cv.THRESH_BINARY)
 
@@ -55,8 +54,5 @@ def generate_patch(source_file: Path,
             cells = cv.countNonZero(th3)
 
             perc = 100 - ((cells / total) * 100)
-            if perc > threshold and len(patches) < 9:
-                patches.append(s)
-            else:
-                return patches
-    return patches
+            if perc > threshold:
+                yield s
